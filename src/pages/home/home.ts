53eeda1766967a825da2bdb3bff8a1d9de92a43a
Eliane Chkayra <=>
2019-07-28 19:39:14 +0300

    toaster

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage ,AlertController} from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -9,7 +10,7 @@ import { NavController, IonicPage ,AlertController} from 'ionic-angular';
 export class HomePage {
 
   my_name: string = 'eliane';
-  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, private toastCtrl: ToastController) {
     const new_name = 'naji';
     this.changeName(new_name, 3000);
   }
@@ -33,4 +34,17 @@ export class HomePage {
     my_alert.present();
   }
 
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Naji wa7ad sa2il',
+      duration: 3000,
+      position: 'middle'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
 }
